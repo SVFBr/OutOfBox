f8091eab45a2cd7dea4387de71d7b4f2ac9cf7b3
SVFBr <sheilafreire.br@gmail.com>
2016-08-30 23:22:19 -0300

    Desafio Merge String

---
+++ MergeString/src/Solution.java
@@ -0,0 +1,40 @@
+/**
+ * Implemente um método que faça o merge de duas strings, que recebendo A e B,
+ * gere um resultado contendo os caracteres alternadamente de cada uma,
+ * começando por A. Se uma das strings for menor, todos os caracteres restantes
+ * da outra deverão ser adicionados no final da string resultante.
+ * 
+ * Para um outra muito interessante e possível linda de pensamento, ver solução
+ * Day6LetsReview no package com.hackerrank.challenges ;)
+ * 
+ */
+
+// a = Acegilnp
+// b = Bdf jm
+// resultado: ABcdefg ijlmn p
+
+public class Solution {
+
+	public static void main(String[] args) {
+
+		String a = "Acegilnp";
+		String b = "Bdf jm ";
+		int iA = 0;
+		int iB = 0;
+		StringBuilder quebra = new StringBuilder();
+
+		for (int i = 0; i <= (a.length() + b.length()); i++) {
+
+			if (iA < a.length()) {
+				quebra.append(a.charAt(iA));
+				iA++;
+			}
+			if (iB < b.length()) {
+				quebra.append(b.charAt(iB));
+				iB++;
+			}
+
+		}
+		System.out.println(quebra);
+	}
+}
