86aeb3cf86766d545d50962dd7fda1ed20f61edb
SVFBr <sheilafreire.br@gmail.com>
2016-09-07 18:44:17 -0300

    Java Loops II

--- Introduction/src/JavaLoopsII.java
+++ Introduction/src/JavaLoopsII.java
@@ -1,25 +1,43 @@
 
+/**
+ *
+ * Sample Input
+2
+0 2 10
+5 3 5
+
+Sample Output
+
+2 6 14 30 62 126 254 510 1022 2046
+8 14 26 50 98
+
+ * */
+
 import java.util.Scanner;
 
 public class JavaLoopsII {
 
 	public static void main(String[] args) {
 
-		Scanner in = new Scanner(System.in);
-		//2
-		int t = in.nextInt();
-		//0 2 10
-		//5 3 5
-		for (int i = 0; i < t; i++) {
-			int a = in.nextInt();
-			int b = in.nextInt();
-			int n = in.nextInt();
+		int a, b, c, n, i = 0;
+		Scanner sc = new Scanner(System.in);
+		n = sc.nextInt();
+
+		while (i < n) {
+			int sum = 0;
+			a = sc.nextInt();
+			b = sc.nextInt();
+			c = sc.nextInt();
+
+			for (int j = 0; j < c; j++) {
+				if (j < 1)
+					sum += a + ((int) Math.pow(2, j) * b);
+				else
+					sum += ((int) Math.pow(2, j) * b);
+				System.out.printf("%s ", sum);
+			}
+			System.out.println();
+			i++;
 		}
-		in.close();
-
 	}
-
 }
-
-//2 6 14 30 62 126 254 510 1022 2046
-//8 14 26 50 98
