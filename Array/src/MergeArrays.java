5136a4b2e82d9f6dcb915755039ddd5625215426
SVFBr <sheilafreire.br@gmail.com>
2016-09-09 04:18:21 -0300

    Implementação do código

--- Array/src/MergeArrays.java
+++ Array/src/MergeArrays.java
@@ -1,5 +1,3 @@
-import java.util.ArrayList;
-
 /**
  * Dada uma sequência de números em um array, retorne esses números ordenados de
  * forma alternada, ou seja, o menor número, seguido do maior, seguido do
@@ -13,12 +11,18 @@ import java.util.ArrayList;
 public class MergeArrays {
 	public static void main(String[] args) {
 
-		int array[] = { 1, 2, 3, 4, 5 };
-		ArrayList  quebra = new ArrayList();
+		int a[] = { 1, 2, 3, 4, 5 };
 
+		int n = a.length;
 
-		System.out.println(quebra);
+		System.out.println("Tamanho do array: " + n);
+		System.out.println("");
 
+		for (int i = 0; i < n - i; i++) {
+
+			System.out.println(a[i]);
+			System.out.println(a[n - (i + 1)] + ".");
+
+		}
 	}
 }
-
